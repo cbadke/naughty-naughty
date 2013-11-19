e3cc700c2fe9c0763d861768fc92d3b1cd8e51c7
Curtis Badke <curtis@badkid.ca>
2013-11-19 13:37:24 -0700

    More warnings

--- naughty.js
+++ naughty.js
@@ -1,7 +1,16 @@
 function myFuntion() {
     var x;
+    var y = 3;
 
     if (x == 2) {
         console.log("you have a 2");
     }
+
+    y = 12;
+
+    if (y = 27)
+    {
+        console.log("i like turtles");
+    }
+    return
 };
